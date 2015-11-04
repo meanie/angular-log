a2953c6ebfbff6717faef2145403108e5c17aa9a
Adam Buczynski <adambuczynski@gmail.com>
2015-11-04 20:46:58 +1300

    Bump version to 1.0.1

--- release/meanie-angular-log.js
+++ release/meanie-angular-log.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-log - v1.0.0 - 23-9-2015
+ * meanie-angular-log - v1.0.1 - 4-10-2015
  * https://github.com/meanie/angular-log
  *
  * Copyright (c) 2015 Adam Buczynski <me@adambuczynski.com>
